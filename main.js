fbc2c400a42574e88f45e6a3d7c3441b88c04df1
Houssem <86334640+Houssem-Ousji@users.noreply.github.com>
2023-02-15 17:25:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process');
+const folderPath = 'C:/Users/houssem.ousji/Desktop/demos/FinalStructure-main';
+const commands = ['npm run firefox','npm run chromium','npm run webkit']; // Replace this with your desired command
+require('dotenv').config()
+commands.map((command)=>{
+    exec(`cd ${process.env.MAIN_SYSTEM_PATH} && ${command}`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Error executing command: ${err}`);
+          return;
+        }
+        console.log(`Command output: ${stdout}`);
+      });
+})
+// 
